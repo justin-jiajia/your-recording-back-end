abdf9788cfeba09f076e90148b1a458f600207b0
jiajia <898247676@qq.com>
2022-09-25 20:07:37 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-from your_recording import create_app
 from dotenv import load_dotenv
 from os import path
 if path.exists('.env'):
     load_dotenv('.env')
 if path.exists('.flaskenv'):
     load_dotenv('.flaskenv')
+from your_recording import create_app
 app = create_app('production')
